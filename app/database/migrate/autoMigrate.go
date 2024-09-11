35674a07a32d567839f1c99f1b77c853488089f0
marcus <mmxo5813@gmail.com>
2024-09-11 23:46:21 +1000

    fix: remove mysql code

--- app/database/migrate/autoMigrate.go
+++ app/database/migrate/autoMigrate.go
@@ -44,24 +44,6 @@ func DropAllTables() error {
 // - Will not change/delete any existing columns and their types
 func StartMigration(configure gconfig.Configuration) error {
 	db := gdatabase.GetDB()
-	configureDB := configure.Database.RDBMS
-	driver := configureDB.Env.Driver
-
-	if driver == "mysql" {
-		// db.Set() --> add table suffix during auto migration
-		if err := db.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(
-			&auth{},
-			&twoFA{},
-			&twoFABackup{},
-			&tempEmail{},
-			&user{},
-		); err != nil {
-			return err
-		}
-
-		fmt.Println("new tables are  migrated successfully!")
-		return nil
-	}
 
 	if err := db.AutoMigrate(
 		&auth{},
