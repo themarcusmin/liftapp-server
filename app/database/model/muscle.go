c1c99002d98b51abe6342cbc129f0d592e56691e
marcus <mmxo5813@gmail.com>
2024-09-14 22:57:22 +1000

    typo

--- app/database/model/muscle.go
+++ app/database/model/muscle.go
@@ -7,5 +7,5 @@ import (
 type Muscle struct {
 	gorm.Model
 	DisplayName string     `json:"displayName,omitempty"`
-	Exercise    []Exercise `gorm:"many2many:exercise_Muscles" json:"exercies,omitempty"`
+	Exercise    []Exercise `gorm:"many2many:exercise_muscles" json:"exercises,omitempty"`
 }
