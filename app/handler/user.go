e25f4a8ccd73577084528445a81ec920ed3db420
marcus <mmxo5813@gmail.com>
2024-09-17 23:34:12 +1000

    cleanup

--- app/handler/user.go
+++ app/handler/user.go
@@ -2,7 +2,6 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
@@ -18,8 +17,6 @@ import (
 func GetUsers() (httpResponse gmodel.HTTPResponse, httpStatusCode int) {
 	db := gdatabase.GetDB()
 	users := []model.User{}
-	// posts := []model.Post{}
-	// hobbies := []model.Hobby{}
 
 	if err := db.Find(&users).Error; err != nil {
 		log.WithError(err).Error("error code: 1101")
@@ -34,17 +31,6 @@ func GetUsers() (httpResponse gmodel.HTTPResponse, httpStatusCode int) {
 		return
 	}
 
-	// for j, user := range users {
-	// 	db.Model(&posts).Where("id_user = ?", user.UserID).Find(&posts)
-	// 	users[j].Posts = posts
-
-	// 	db.Model(&hobbies).Joins("JOIN user_hobbies ON user_hobbies.hobby_hobby_id=hobbies.hobby_id").
-	// 		Joins("JOIN users ON users.user_id=user_hobbies.user_user_id").
-	// 		Where("users.user_id = ?", user.UserID).
-	// 		Find(&hobbies)
-	// 	users[j].Hobbies = hobbies
-	// }
-
 	httpResponse.Message = users
 	httpStatusCode = http.StatusOK
 	return
@@ -54,24 +40,12 @@ func GetUsers() (httpResponse gmodel.HTTPResponse, httpStatusCode int) {
 func GetUser(id string) (httpResponse gmodel.HTTPResponse, httpStatusCode int) {
 	db := gdatabase.GetDB()
 	user := model.User{}
-	// posts := []model.Post{}
-	// hobbies := []model.Hobby{}
 
 	if err := db.Where("user_id = ?", id).First(&user).Error; err != nil {
 		httpResponse.Message = "user not found"
 		httpStatusCode = http.StatusNotFound
 		return
 	}
-
-	// db.Model(&posts).Where("id_user = ?", user.UserID).Find(&posts)
-	// user.Posts = posts
-
-	// db.Model(&hobbies).Joins("JOIN user_hobbies ON user_hobbies.hobby_hobby_id=hobbies.hobby_id").
-	// 	Joins("JOIN users ON users.user_id=user_hobbies.user_user_id").
-	// 	Where("users.user_id = ?", user.UserID).
-	// 	Find(&hobbies)
-	// user.Hobbies = hobbies
-
 	httpResponse.Message = user
 	httpStatusCode = http.StatusOK
 	return
@@ -88,8 +62,6 @@ func CreateUser(userIDAuth uint64, user model.User) (httpResponse gmodel.HTTPRes
 		httpStatusCode = http.StatusForbidden
 		return
 	}
-	fmt.Println("create user 0 :", userFinal)
-	fmt.Println("create user 1 :", userFinal.FirstName)
 
 	// user must not be able to manipulate all fields
 	userFinal.FirstName = user.FirstName
@@ -142,53 +114,3 @@ func UpdateUser(userIDAuth uint64, user model.User) (httpResponse gmodel.HTTPRes
 	httpStatusCode = http.StatusOK
 	return
 }
-
-// AddHobby handles jobs for controller.AddHobby
-// func AddHobby(userIDAuth uint64, hobby model.Hobby) (httpResponse gmodel.HTTPResponse, httpStatusCode int) {
-// 	db := gdatabase.GetDB()
-// 	user := model.User{}
-// 	hobbyNew := model.Hobby{}
-// 	hobbyFound := 0 // default (do not create new hobby) = 0, create new hobby = 1
-
-// 	// does the user have an existing profile
-// 	if err := db.Where("id_auth = ?", userIDAuth).First(&user).Error; err != nil {
-// 		httpResponse.Message = "no user profile found"
-// 		httpStatusCode = http.StatusForbidden
-// 		return
-// 	}
-
-// 	if err := db.Where("hobby = ?", hobby.Hobby).First(&hobbyNew).Error; err != nil {
-// 		hobbyFound = 1 // create new hobby
-// 	}
-
-// 	if hobbyFound == 1 {
-// 		hobbyNew.Hobby = hobby.Hobby
-// 		tx := db.Begin()
-// 		if err := tx.Create(&hobbyNew).Error; err != nil {
-// 			tx.Rollback()
-// 			log.WithError(err).Error("error code: 1131")
-// 			httpResponse.Message = "internal server error"
-// 			httpStatusCode = http.StatusInternalServerError
-// 			return
-// 		}
-// 		tx.Commit()
-// 		hobbyFound = 0
-// 	}
-
-// 	if hobbyFound == 0 {
-// 		user.Hobbies = append(user.Hobbies, hobbyNew)
-// 		tx := db.Begin()
-// 		if err := tx.Save(&user).Error; err != nil {
-// 			tx.Rollback()
-// 			log.WithError(err).Error("error code: 1132")
-// 			httpResponse.Message = "internal server error"
-// 			httpStatusCode = http.StatusInternalServerError
-// 			return
-// 		}
-// 		tx.Commit()
-// 	}
-
-// 	httpResponse.Message = user
-// 	httpStatusCode = http.StatusOK
-// 	return
-// }
